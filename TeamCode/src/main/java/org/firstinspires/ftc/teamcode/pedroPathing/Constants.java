95f9dfc7e1a65523a0aae09ced7936bfb3fe05bc
Ricenoodles21 <aveionvang@gmail.com>
2025-10-01 16:00:59 -0400

    Update config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -16,7 +16,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(10.1);
+            .mass(11);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
