eb78b55c2331414839fdd770c755c7c1862b9389
DylDev <dylanb92010@gmail.com>
2025-10-24 22:22:38 -0400

    Fixex and improvements in launcher test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LauncherTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LauncherTest.java
@@ -13,9 +13,10 @@ import org.firstinspires.ftc.teamcode.utils.Constants.LauncherConstants.Launcher
 /*
 This is a simple test OpMode for the launcher. It uses the custom Launcher helper (imported above).
 
-To start the launcher, press the right bumper on gamepad 1. This will run the launcher update loop to launch 3 artifacts.
-If at any point, the launcher needs to be stopped, press the right bumper on gamepad 1 again, this will return the launcher to an idle state.
-To adjust the target RPM of the launcher, use the dpad up and down buttons on gamepad 1.
+To start the launcher, press the left bumper on gamepad 1. This will get the launcher up to the target RPM..
+Press the left bumper again to stop the launcher.
+To launch artifacts, press the left trigger to launch 1 artifact, or the right trigger to launch 3 artifacts.
+NOTE: You can launch without speeding up, it will be sped up automatically and then it will shut back down.
 */
 
 @TeleOp(name = "Launcher Test", group = "Tests")
@@ -23,6 +24,7 @@ To adjust the target RPM of the launcher, use the dpad up and down buttons on ga
 public class LauncherTest extends LinearOpMode {
     private Launcher launcher; // Custom launcher class
     private LauncherReturnProps launcherStatus; // Current launcher state
+    private boolean launcherIsActive = false; // True when launcher is active (speed up)
     private boolean launcherIsLaunching = false; // True when launcher is launching
     private final ElapsedTime launchCycleTimer = new ElapsedTime(); // Keeps track of the time since the last completed launch cycle
 
@@ -40,8 +42,14 @@ public class LauncherTest extends LinearOpMode {
 
         while (opModeIsActive()) {
             // If the left bumper is pressed and the launcher is idle
-            if (gamepad1.leftBumperWasPressed() && launcherStatus.state == LauncherState.IDLE) {
-                launcher.speedUp(); // Speed up the launcher
+            if (gamepad1.leftBumperWasPressed()) {
+                if (launcherIsActive) {
+                    launcher.stop(); // Stop the launcher
+                    launcherIsActive = false; // Set active flag to false
+                } else {
+                    launcher.speedUp(); // Speed up the launcher
+                    launcherIsActive = true; // Set active flag to true
+                }
             }
 
             // If the launcher is running, we don't want to command more
@@ -49,21 +57,16 @@ public class LauncherTest extends LinearOpMode {
                 // If the left trigger is pressed, launch an artifact
                 if (gamepad1.left_trigger > 0.5) {
                     launcher.launch(1); // Command the launcher to launch 1 artifact
+                    launcherIsActive = true; // Set active flag to true (may already be true)
                     launcherIsLaunching = true; // Set launching flag to true
                 }
 
                 // If the right trigger is pressed, launch 3 artifacts
                 if (gamepad1.right_trigger > 0.5) {
                     launcher.launch(3); // Command the launcher to launch 3 artifacts
+                    launcherIsActive = true; // Set active flag to true (may already be true)
                     launcherIsLaunching = true; // Set launching flag to true
                 }
-            }
-
-            // Use DPad buttons to control launcher speed
-            if (gamepad1.dpadUpWasPressed()) {
-                launcher.setTargetRPM(launcher.getTargetRPM() + 25); // Increase target RPM by 25
-            } else if (gamepad1.dpadDownWasPressed()) {
-                launcher.setTargetRPM(launcher.getTargetRPM() - 25); // Decrease target RPM by 25
             }
 
             // Update launcher
@@ -73,6 +76,10 @@ public class LauncherTest extends LinearOpMode {
             if (launcherStatus.cycleCompleted) {
                 launchCycleTimer.reset(); // Reset launch cycle timer
                 launcherIsLaunching = false; // Reset launching flag
+
+                if (launcherStatus.state == LauncherState.IDLE) {
+                    launcherIsActive = false; // If the launcher was stopped after launching, set active flag to false
+                }
             }
 
             // Log status
