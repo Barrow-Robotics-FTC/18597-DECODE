ba3a6890860689e11837557904892bce32799285
DylDev <dylanb92010@gmail.com>
2025-12-10 06:20:01 -0500

    WhackerTest OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/WhackerTest.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.Robot;
+
+/*
+Position Recorder OpMode
+Displays the current position of the robot in telemetry to help record positions for autonomous paths
+Start the robot in the blue alliance human player zone corner (localize position)
+*/
+
+@TeleOp(name = "Whacker Test", group = "Tests")
+@SuppressWarnings("FieldCanBeLocal") // Suppress pointless Android Studio warnings
+public class WhackerTest extends LinearOpMode {
+    private Robot robot; // Custom robot class
+
+    @Override
+    public void runOpMode() {
+        // Create instance of launcher and initialize
+        robot = new Robot(hardwareMap, Constants.Mode.TELEOP);
+
+        // Log completed initialization
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for START to be pressed
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Update robot
+            robot.update(gamepad1, gamepad2);
+
+            if (gamepad1.rightBumperWasPressed()) {
+                if (!robot.whacker.isWhacking()) {
+                    robot.whacker.whack();
+                }
+            }
+
+            // Log status
+            telemetry.addData("Whacker State", robot.whacker.getState());
+            telemetry.update();
+        }
+    }
+}
